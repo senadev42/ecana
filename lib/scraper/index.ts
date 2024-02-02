b4946c76144c03c9c094f50a05943a38fc1c0d2c
senadev42 <senadevv42@gmail.com>
2024-02-02 17:35:23 +0300

    types

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -5,24 +5,28 @@ import * as cheerio from "cheerio";
 //getting out redirect hell
 import { wrapper } from "axios-cookiejar-support";
 import { CookieJar } from "tough-cookie";
+import { SingleEntry } from "@/types";
 
 export async function scrapeCommodities(url: string) {
   if (!url) return;
 
+  console.log(new Date().toISOString(), " Starting scraping...");
   //getting around the redirect issues
   const jar = new CookieJar();
   const client = wrapper(axios.create({ jar }));
-  
+
   //optional proxying if the site throws a fit
 
   try {
     const response = await client.get(url);
     const $ = cheerio.load(response.data);
 
+    const type = "coffee";
     const date = $("#MainContent_lblCoffee").text().trim();
+    const transformedDate = new Date(date).toISOString().split("T")[0];
 
-    const coffeeData: any[] = [];
-
+    //coffee
+    const coffeeData: SingleEntry[] = [];
     $("#MainContent_gvCoffee tr:gt(0)").each((index, element) => {
       const columns = $(element).find("td");
 
@@ -30,26 +34,28 @@ export async function scrapeCommodities(url: string) {
         symbol: columns.eq(0).text().trim(),
         warehouseCode: columns.eq(1).text().trim(),
         productionYear: columns.eq(2).text().trim(),
-        prevClose: columns.eq(3).text().trim(),
-        close: columns.eq(4).text().trim(),
-        change: columns.eq(5).text().trim(),
-        high: columns.eq(6).text().trim(),
-        low: columns.eq(7).text().trim(),
-        volume: columns.eq(8).text().trim(),
+        prevClose: Number(columns.eq(3).text().trim()),
+        close: Number(columns.eq(4).text().trim()),
+        change: Number(columns.eq(5).text().trim()),
+        high: Number(columns.eq(6).text().trim()),
+        low: Number(columns.eq(7).text().trim()),
+        volume: Number(columns.eq(8).text().trim()),
       };
 
       coffeeData.push(coffeeInfo);
     });
-    
 
-
+    //the other commodities once this passes the proof of concept stage
 
     const result = {
-      date: date,
-      coffeeData: coffeeData,
+      type: type,
+      data: coffeeData,
+      date: transformedDate,
+      recordID: `${type}${transformedDate}`
     };
 
-    console.log(result);
+    console.log(new Date().toISOString(), " Done Scraping");
+    return result;
   } catch (error) {
     console.log(error);
   }
